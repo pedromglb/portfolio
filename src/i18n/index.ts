558a6f50f154517aa1b274181bd9a84e6f6b1ce5
Pedro Baptista <pedromiguel_lag@hotmail.com>
2026-01-07 22:59:45 +0000

    1st Phase Portfolio

---
+++ src/i18n/index.ts
@@ -0,0 +1,17 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import en from './locales/en/translation.json';
+import pt from './locales/pt/translation.json';
+import de from './locales/de/translation.json';
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: { en, pt, de },
+    lng: 'en',
+    fallbackLng: 'en',
+    interpolation: { escapeValue: false },
+  });
+
+export default i18n;
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
